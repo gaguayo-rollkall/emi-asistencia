dc0a04698acb19eada5e72e9bdf2d0c32dbdd1be
Gonzalo Aguayo <gaguayo@rollkall.com>
2024-04-04 22:06:12 -0400

    fake

--- web-cliente/src/paginas/reportes/RegistrosEvento.jsx
+++ web-cliente/src/paginas/reportes/RegistrosEvento.jsx
@@ -5,9 +5,10 @@ import { ComboBoxComponent } from '@syncfusion/ej2-react-dropdowns';
 
 import Breadcrumbs from '../../components/Breadcrumbs';
 import apiService from '../../servicios/api-service';
+import { reportFake } from './fake/alumnos';
 
 export default function RegistrosEvento() {
-  const [reporte, setReporte] = useState([]);
+  const [reporte, setReporte] = useState(reportFake);
   const [carreras, setCarreras] = useState([])
   const [gestiones, setGestiones] = useState([])
   const [periodos, setPeriodos] = useState({})
@@ -135,53 +136,11 @@ export default function RegistrosEvento() {
                       <p className="text-base mt-4">{nombre}</p>
 
                       <GridComponent dataSource={estudiantes}
-                        allowPaging={false}
-                        rowTemplate={({ codigo, nombre, asistencias }) => (
-                          <tr className="e-row">
-                            <td className="e-rowcell">
-                              {codigo}
-                            </td>
-                            <td className="e-rowcell">
-                              {nombre}
-                            </td>
-                            <td className="e-rowcell">
-                              <ul className="timeline timeline-vertical">
-                                {asistencias.map(({ asistencia, dia }, index) => (
-                                  <li key={index}>
-                                    {index > 0 && <hr />}
-                                    <div className="timeline-start">{dia}</div>
-                                    <div className="timeline-middle">
-                                      {asistencia ? (
-                                        <svg
-                                          viewBox="0 0 1024 1024"
-                                          fill="green"
-                                          height="1em"
-                                          width="1em"
-                                        >
-                                          <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z" />
-                                        </svg>
-                                      ) : (
-                                        <svg
-                                          viewBox="0 0 1024 1024"
-                                          fill="red"
-                                          height="1em"
-                                          width="1em"
-                                        >
-                                          <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z" />
-                                        </svg>
-                                      )}
-                                    </div>
-                                    <div className="timeline-end timeline-box">{asistencia ? 'Si' : 'No'}</div>
-                                    {index < asistencias.length - 1 && <hr />}
-                                  </li>
-                                ))}
-                              </ul>
-                            </td>
-                          </tr>)}>
+                        allowPaging={false}>
                         <ColumnsDirective>
                           <ColumnDirective field='codigo' headerText='Codigo' isPrimaryKey={true} />
                           <ColumnDirective field='nombre' headerText='Nombre' />
-                          <ColumnDirective field='asistencias' headerText='Asistencias' />
+                          <ColumnDirective field='registros' headerText='Asistencias' />
                         </ColumnsDirective>
                         <Inject services={[Page, Toolbar, Edit]} />
                       </GridComponent>
